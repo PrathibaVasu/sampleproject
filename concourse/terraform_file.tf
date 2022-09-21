fdbd0d0f3adb9e8754e27f470db4620b36818038
prathibavasu <prathiba.vasu@quantiphi.com>
2022-09-21 17:17:21 +0530

    Modified glue job

--- concourse/terraform_file.tf
+++ concourse/terraform_file.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 
 resource "aws_glue_job" "example_job" {
-  name              = "terraform_example_job"
+  name              = "terraform_test_pipeline_job"
   role_arn          = "arn:aws:iam::298841451579:role/AWSPractice-Developer"
   worker_type         = "G.1X"
   number_of_workers = var.no_of_workers
@@ -21,7 +21,6 @@ resource "aws_glue_job" "example_job" {
     #"--continuous-log-logGroup"          = aws_cloudwatch_log_group.example.name
     #"--enable-continuous-cloudwatch-log" = "true"
     #"--enable-continuous-log-filter"     = "true"
-    #"--enable-metrics"                   = ""
   }
 }
 
